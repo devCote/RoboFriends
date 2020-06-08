c1d06e5d8f7b54496494e5a9003848278829d689
DmitriyBakhanenko <bahanenko@gmail.com>
2020-06-08 22:36:35 +0300

    v1.2

--- src/Card.js
+++ src/Card.js
@@ -1,9 +1,15 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-class Card extends Component {
-  render() {
-    return <h1>hello</h1>;
-  }
-}
+const Card = () => {
+  return (
+    <div className="bg-light-green dib br3 pa2 ma2 tc grow bw2 shadow-5">
+      <img src="https://robohash.org/set_set5/test?size=200x200" alt="img" />
+      <div>
+        <h2>John Doe</h2>
+        <p>email@mail.com</p>
+      </div>
+    </div>
+  );
+};
 
 export default Card;
