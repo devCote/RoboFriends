6f33cbb3d965059125bb4b50f61662ae0f9f9a5c
DmitriyBakhanenko <bahanenko@gmail.com>
2020-06-08 23:49:37 +0300

    v0.0.4 change name

--- src/Card.js
+++ src/Card.js
@@ -5,7 +5,7 @@ const Card = ({ id, name, email }) => {
     <div className="bg-light-green dib br3 pa2 ma2 tc grow bw2 shadow-5">
       <img src={`https://robohash.org/set_set2/${id}?size=200x200`} alt="img" />
       <div>
-        <h2>{name}</h2>
+        <h3>{name}</h3>
         <p>{email}</p>
       </div>
     </div>
